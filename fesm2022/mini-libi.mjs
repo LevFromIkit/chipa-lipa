39cd866a00b283f2fbe9fa988d8e071adcd7260c
LevFromIKIT <levpalc@gmail.com>
2025-11-11 23:32:39 +0700

    f

---
+++ fesm2022/mini-libi.mjs
@@ -0,0 +1,39 @@
+import * as i0 from '@angular/core';
+import { Component } from '@angular/core';
+
+class MiniLibi {
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.10", ngImport: i0, type: MiniLibi, deps: [], target: i0.ɵɵFactoryTarget.Component });
+    static ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "20.3.10", type: MiniLibi, isStandalone: true, selector: "ttl-mini-libi", ngImport: i0, template: `
+    <p>
+      mini-libi works!!!!!
+    </p>
+  `, isInline: true, styles: [""] });
+}
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.10", ngImport: i0, type: MiniLibi, decorators: [{
+            type: Component,
+            args: [{ selector: 'ttl-mini-libi', imports: [], standalone: true, template: `
+    <p>
+      mini-libi works!!!!!
+    </p>
+  ` }]
+        }] });
+
+class KokralComponent {
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.10", ngImport: i0, type: KokralComponent, deps: [], target: i0.ɵɵFactoryTarget.Component });
+    static ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "20.3.10", type: KokralComponent, isStandalone: true, selector: "ttl-kokral", ngImport: i0, template: "<p>kokral.component works!</p>\r\n", styles: [""] });
+}
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.10", ngImport: i0, type: KokralComponent, decorators: [{
+            type: Component,
+            args: [{ selector: 'ttl-kokral', imports: [], standalone: true, template: "<p>kokral.component works!</p>\r\n" }]
+        }] });
+
+/*
+ * Public API Surface of mini-libi
+ */
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { KokralComponent, MiniLibi };
+//# sourceMappingURL=mini-libi.mjs.map
---
+++ index.d.ts
@@ -0,0 +1,13 @@
+import * as i0 from '@angular/core';
+
+declare class MiniLibi {
+    static ɵfac: i0.ɵɵFactoryDeclaration<MiniLibi, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<MiniLibi, "ttl-mini-libi", never, {}, {}, never, never, true, never>;
+}
+
+declare class KokralComponent {
+    static ɵfac: i0.ɵɵFactoryDeclaration<KokralComponent, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<KokralComponent, "ttl-kokral", never, {}, {}, never, never, true, never>;
+}
+
+export { KokralComponent, MiniLibi };
