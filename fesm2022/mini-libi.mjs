ec36a3e582c2190eacb6b7e918d77cfe5101f168
LevFromIKIT <levpalc@gmail.com>
2025-11-11 23:04:06 +0700

    fd

---
+++ fesm2022/mini-libi.mjs
@@ -0,0 +1,30 @@
+import * as i0 from '@angular/core';
+import { Component } from '@angular/core';
+
+class MiniLibi {
+    static ɵfac = i0.ɵɵngDeclareFactory({ minVersion: "12.0.0", version: "20.3.10", ngImport: i0, type: MiniLibi, deps: [], target: i0.ɵɵFactoryTarget.Component });
+    static ɵcmp = i0.ɵɵngDeclareComponent({ minVersion: "14.0.0", version: "20.3.10", type: MiniLibi, isStandalone: true, selector: "ttl-mini-libi", ngImport: i0, template: `
+    <p>
+      mini-libi works!!!!!
+    </p>
+  `, isInline: true, styles: [""] });
+}
+i0.ɵɵngDeclareClassMetadata({ minVersion: "12.0.0", version: "20.3.10", ngImport: i0, type: MiniLibi, decorators: [{
+            type: Component,
+            args: [{ selector: 'ttl-mini-libi', imports: [], standalone: true, template: `
+    <p>
+      mini-libi works!!!!!
+    </p>
+  ` }]
+        }] });
+
+/*
+ * Public API Surface of mini-libi
+ */
+
+/**
+ * Generated bundle index. Do not edit.
+ */
+
+export { MiniLibi };
+//# sourceMappingURL=mini-libi.mjs.map
