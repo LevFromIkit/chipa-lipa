3fd7255dbfd4d084a85d6a7288df7a3eb6e6ecf3
LevFromIKIT <levpalc@gmail.com>
2025-11-11 22:58:20 +0700

    ddd

---
+++ dist/mini-libi/index.d.ts
@@ -0,0 +1,8 @@
+import * as i0 from '@angular/core';
+
+declare class MiniLibi {
+    static ɵfac: i0.ɵɵFactoryDeclaration<MiniLibi, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<MiniLibi, "ttl-mini-libi", never, {}, {}, never, never, true, never>;
+}
+
+export { MiniLibi };
